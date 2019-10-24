d774e05f0578941caefe40ece396a6ad751a0697
gabrieldpg <gabrielpcpg@gmail.com>
2019-10-25 09:36:33 +1300

    Initial Setup, functions created

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool gameOver;
+
+void Setup() {
+
+}
+
+void Draw() {
+
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
+int main() {
+
+	return 0;
+}
