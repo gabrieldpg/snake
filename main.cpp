be6cb30f6695527ade5c30e566b53782c6dbf587
gabrieldpg <gabrielpcpg@gmail.com>
2019-10-25 10:15:16 +1300

    Logic function written

--- main.cpp
+++ main.cpp
@@ -78,6 +78,23 @@ void Input() {
 
 void Logic() {
 
+	switch(dir) {
+		case LEFT:
+			x--;
+			break;
+		case RIGHT:
+			x++;
+			break;
+		case UP:
+			y--;
+			break;
+		case DOWN:
+			y++;
+			break;
+		default:
+			break;
+	}
+
 }
 
 int main() {
