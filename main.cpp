473663443ec1ba03ccc31323506c7747efa8e2cd
gabrieldpg <gabrielpcpg@gmail.com>
2019-10-25 09:41:12 +1300

    Added global variables and simple main logic

--- main.cpp
+++ main.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 
 bool gameOver;
+const int width = 20;
+const int height = 20;
+int x,y,fruitX,fruitY,score;
+enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
 
 void Setup() {
-
+	gameOver = false;
 }
 
 void Draw() {
@@ -20,6 +25,15 @@ void Logic() {
 }
 
 int main() {
+
+	Setup();
+
+	while (!gameOver) {
+		Draw();
+		Input();
+		Logic();
+		//Sleep(); can be used to slow game down
+	}
 
 	return 0;
 }
